c80873e2e899e80976ec7ef593a8dd17f450e48c
YaniaNada <166074902+YaniaNada@users.noreply.github.com>
2026-01-11 17:06:59 +0100

    updated script to clean the code and correct logic

--- exercise_2a/main.py
+++ exercise_2a/main.py
@@ -1,6 +1,5 @@
 import argparse
-from exercise_2a.multiple_class_objects import UserInfo,  ModelObject
-
+from exercise_2a.greeting_message import UserInfo, greet_user, write_text
 
 def user_greeting():
 
@@ -15,49 +14,19 @@ def user_greeting():
     user= UserInfo(args.name, args.age) 
 
     # Greet user with a message.
-    greeting= UserInfo.greet_user(user)
+    greeting= greet_user(user)
     
     # Store the greeting message in a file.
     file_path= args.filepath
-    UserInfo.write_text(greeting, file_path)
+    write_text(greeting, file_path)
 
     # Display the greeting message.
     print(greeting)
 
 
-
-def prediction_model():
-
-    # Create ArgumentParser object.
-    model_parser= argparse.ArgumentParser(description= 'Predict with linear regression model')
-    model_parser.add_argument('-beds', type= int, help= 'Total number of beds')
-    model_parser.add_argument('-baths', type= int, help= 'Total number of bathrooms')
-    model_parser.add_argument('-size', type= float, help= 'Size of the house floor in square feet')
-    model_parser.add_argument('-zip_code', type= str, help= 'The zip code of the location')
-    model_args= model_parser.parse_args()
-
-    # Initialize feature values.
-    X_new = [model_args.beds, model_args.baths, model_args.size, model_args.zip_code]
-
-    # Make prediction.
-    predicted_price= ModelObject.call_predict(X_new)
-    
-    # Display the prediction
-    print(f' X: {X_new}, /n predicted price: {predicted_price}')
-
-
 def main():
-
-    user_input = input("Enter 'a' to receive a greeting message or 'b' to use the predictive model")
-    
-    if user_input == 'a':
-        print("Run the main.py file with your name and age")
-        user_greeting()
-
-    elif user_input== 'b':
-        print("Run the main.py file with these values: number of beds available, number of bathrooms, floor size of the house, and zip code of your area")
-        prediction_model()
+    user_greeting()
 
 
 if __name__== "__main__":
-    main()
+    main()  
