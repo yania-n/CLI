b225367b7611142e70d0b4ab58b475cac6c817b9
YaniaNada <166074902+YaniaNada@users.noreply.github.com>
2026-01-17 19:57:44 +0100

    updated script to create the arg parser as standalone parser that can be used as parent

--- exercise_2a/main.py
+++ exercise_2a/main.py
@@ -1,32 +1,32 @@
 import argparse
 from exercise_2a.greeting_message import UserInfo, greet_user, write_text
 
-def user_greeting():
-
+def get_parser():
     # Create ArgumentParser object.
-    parser= argparse.ArgumentParser(description= 'Get user info and greet user')
+    parser= argparse.ArgumentParser(description= 'Get user info and greet user', add_help = False)
     parser.add_argument('--name', type= str, default= 'Yania', help= 'user name')
     parser.add_argument('--age', type= int, default= 30, help= 'age of the user')    
     parser.add_argument('--filepath', default= 'exercise_2a/output.txt', help= 'destination file path')
-    args= parser.parse_args()
+    return parser
+
+def user_greeting(args):
 
     # Create user object.
     user= UserInfo(args.name, args.age) 
 
     # Greet user with a message.
     greeting= greet_user(user)
-    
-    # Store the greeting message in a file.
-    file_path= args.filepath
-    write_text(greeting, file_path)
 
     # Display the greeting message.
     print(greeting)
 
-
-def main():
-    user_greeting()
-
+    return greeting
 
 if __name__== "__main__":
-    main()  
+    standalone_parser = argparse.ArgumentParser(parents = [get_parser()])
+    args = standalone_parser.parse_args()
+    greeting = user_greeting(args)
+    
+    # Store the greeting message in a file.
+    file_path= args.filepath
+    write_text(greeting, file_path)
