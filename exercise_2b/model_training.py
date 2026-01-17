654c5e5202930ee2bf3bdd29419d89aa5f952e1f
YaniaNada <166074902+YaniaNada@users.noreply.github.com>
2026-01-17 18:04:46 +0100

    training model with dataset

--- exercise_2b/model_training.py
+++ exercise_2b/model_training.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from exercise_2b.split_dataset import split_dataset
 from exercise_2b.model_initialization import ModelObject
-from exercise_1a.write_function import write_fn
 
 # Load dataset
 data = pd.read_csv('exercise_2b\dataset.csv')
@@ -11,22 +10,21 @@ df = pd.DataFrame(data)
 X = df[['beds', 'baths', 'size', 'zip_code']]
 y = df['price']
 
-
 X_train, X_test, y_train, y_test = split_dataset(X, y)
 
-if __name__ == '__main__':
-    y_pred = ModelObject.train_model(ModelObject, X_train, X_test, y_train)
-    mse, r2 = ModelObject.evaluate(y_test, y_pred)
+ModelObject.model_fit(X_train, y_train)
+y_pred = ModelObject.call_predict(X_test)
+mse, r2 = ModelObject.evaluate(y_test, y_pred)
 
+def main():
     # Display result
     df = X_test
     df['Actual'] = y_test
     df['Predicted'] = y_pred
     print(f' RESULT:')
-    print(f'{df}')
+    print(f'\n{df}')
     print(f'Mean square error: {mse}')
     print(f'R-squared Score: {r2}')
-
-    # Write result to a file
-    output = f'Mean square error: {mse}, R-squared Score: {r2}'
-    write_fn(output, 'exercise_2b/model_output_test.txt')
+    
+if __name__ == '__main__':
+    main()
